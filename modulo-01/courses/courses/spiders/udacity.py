3babbf91240f06f53b71fded6ce6099187a62642
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-01-22 17:19:49 -0200

    aula 36

--- modulo-01/courses/courses/spiders/udacity.py
+++ modulo-01/courses/courses/spiders/udacity.py
@@ -11,14 +11,33 @@ class UdacitySpider(scrapy.Spider):
         for div in divs:
             link = div.xpath('.//h3/a')
             # test() pega apensa o texto 
-            title = link.xpath('./text()').extract_first()
+            # title = link.xpath('./text()').extract_first()
             # @ pega o atributo da tag
             href = link.xpath('./@href').extract_first()
-            img = div.xpath('.//img[contains(@class, "img-responsive")]/@src').extract_first()
-            description = div.xpath('.//div[2]/div[2]/span/text()').extract_first()
-            yield {
-                'title' : title,
-                'url': href,
-                'image': img,
-                'description': description,
-            }
+            # img = div.xpath('.//img[contains(@class, "img-responsive")]/@src').extract_first()
+            # description = div.xpath('.//div[2]/div[2]/span/text()').extract_first()
+            yield scrapy.Request(
+                url= 'https://br.udacity.com%s' %href, 
+                callback=self.parse_detail
+            )
+
+    def parse_detail(self, response):
+        title = response.xpath(
+            '//h1[contains(@class, "hero__course--title")]/text()'
+        ).extract_first()
+        readline = response.xpath(
+            '//div[contains(@class, "summary-text")]/p/text()'
+        ).extract_first()
+        instructors = []
+        for div in response.xpath('//ir-instructor'):
+            instructors.append(
+                {
+                    'name': div.xpath('./h3/text()').extract_first(),
+                    'image': div.xpath('.//img/@src').extract_first()
+                }
+            )
+        yield {
+            'title' : title,
+            'readline': readline,
+            'instructor': instructors,
+        }
