0a9abfa1cea68f8dc598c4d0f6a9b45a1de8a0cd
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-01-30 14:00:54 -0200

    Aula 56

--- modulo-03/research/research/spiders/ieee.py
+++ modulo-03/research/research/spiders/ieee.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from scrapy.spider import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-class IeeeSpider(scrapy.Spider):
+class IeeeSpider(CrawlSpider):
     name = 'ieee'
     allowed_domains = ['www.ieee.org']
     start_urls = ['https://www.ieee.org/conferences_events/conferences/search/index.html']
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule (
+            LinkExtractor(allow='/conferences_events/conferences/search/index.html')
+        ),
+        Rule (
+            LinkExtractor(
+                allow='/conferences_events/conferences/conferencedetails/index.html'                
+            ),  callback='parse_conferece'
+        )
+    )
+
+    def parse_conferece(self, response):
+        self.log(response.xpath("//title/text()").extract_first())
